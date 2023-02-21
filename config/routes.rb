90638f2ddf15187d440ba9e900ae2e1ab7b3f7b5
max reid <maxwilliamreid@gmail.com>
2023-02-21 15:12:33 +1100

    havent pushed in a while, adding bulk of work

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,19 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # view a list of all restaurants
+  # get 'restaurants', to: 'restaurants#index'
+  # # enter info about a new restaurant and add it to database
+  # get 'restaurants/new', to: 'restaurants#new'
+  # post 'restaurants', to: 'restaurants#create'
+  # # see the details of an individual restaurant
+  # get 'restaurants/:id', to: 'restaurants#show', as: :single_restaurant
+  # # enter a review about a restaurant and add it to database
+  # post 'restaurants/:id/reviews/new', to: 'reviews#create'
+  # get 'restaurants/:restaurant_id/reviews', to: 'reviews#new', as: :new_restaurant_review
+
+  resources :restaurants do
+    resources :reviews, only: [:create, :new]
+  end
+  resources :reviews, only: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.destroy_all
+
+5.times do
+  puts "generating random data!"
+  categories = ["chinese", "italian", "japanese", "french", "belgian"]
+  name = Faker::Restaurant.name
+  address = Faker::Address.full_address
+  category = categories[rand(0..4)]
+  phone_number = Faker::PhoneNumber.phone_number
+  puts "storing in hash"
+  hash = { name: name, address: address, phone_number: phone_number, category: category }
+  puts "Restaurant.new!"
+  restaurant = Restaurant.new(hash)
+  puts "Restaurant.save!"
+  restaurant.save
+end
